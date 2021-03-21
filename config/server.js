07674698fda6bb648fe84c5b35f89c8bd3399967
tomiwa <sannimichaelse@gmail.com>
2021-03-21 22:02:39 +0100

    initial commit to video-api-backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db466010d5ff3ca98abb8354edd59541'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3488cf2e-7be7-46f4-8a1e-5cb4a2db7c27'
+};
